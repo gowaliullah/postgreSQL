1ff0863e664ab7216aec51615907fa9c28caa3ed
MD Waliullah <waliullah.godev@gmail.com>
2025-08-18 13:00:42 +0000

    max, min and sum

--- select.sql
+++ select.sql
@@ -97,3 +97,8 @@ SELECT length(first_name) FROM students;
 
 -- get avg of the ages
 SELECT avg(age) FROM students;
+
+
+SELECT max(age) FROM students;
+SELECT min(age) FROM students;
+SELECT sum(age) FROM students;
