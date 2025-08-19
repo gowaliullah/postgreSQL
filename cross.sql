2560914a8ee7b6a66c62809339e876e184c8ef6f
MD Waliullah <waliullah.godev@gmail.com>
2025-08-19 11:55:59 +0000

    create more 2 tables for practice join

---
+++ cross.sql
@@ -0,0 +1,20 @@
+CREATE Table employees (
+    emp_id INT,
+    emp_name VARCHAR(25),
+    dept_id INT
+)
+
+CREATE Table departments (
+    dpt_id INT,
+    dpt_name VARCHAR(50)
+)
+
+INSERT INTO employees VALUES(1, 'SEI TMI', 101);
+INSERT INTO employees VALUES(2, 'KNO HAI', 102);
+
+INSERT INTO departments VALUES(101, 'HR');
+INSERT INTO departments VALUES(102, 'MK');
+
+
+SELECT * FROM employees;
+SELECT * FROM departments;
