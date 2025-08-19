b9d1685ad65f24f91a10d48f31cd89d9a53b8bea
MD Waliullah <waliullah.godev@gmail.com>
2025-08-19 08:18:31 +0000

    get current data, time

--- date.sql
+++ date.sql
@@ -9,3 +9,11 @@ INSERT INTO timeZ VALUES('2024-01-12 10:45:00', '2024-01-12 10:45:00')
 
 
 SELECT * FROM timeZ;
+
+
+SELECT now();
+
+
+SELECT CURRENT_DATE;
+SELECT now()::date;
+SELECT now()::time;
