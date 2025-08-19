b2e0262ae0a1a2710061250a5c7b463a4996ec4f
MD Waliullah <waliullah.godev@gmail.com>
2025-08-19 09:46:31 +0000

    extract, groupBy, having

--- groupBy.sql
+++ groupBy.sql
@@ -1,5 +1,7 @@
 SELECT * FROM students;
 
+-- Group By -> Split, Apply, Combine
+
 
 SELECT country FROM students
         GROUP BY country;
@@ -7,4 +9,20 @@ SELECT country FROM students
 
         SELECT country, count(*) FROM students
         GROUP BY country;
+
+        SELECT country, count(*), avg(age) FROM students
+        GROUP BY country;
+
+
+        -- having run hoy every GROUP ar against e
+        SELECT country, avg(age) FROM students
+        GROUP BY country
+            HAVING avg(age) > 15
+        ;
+
+
+    SELECT extract(year from dob) as birth_year, count(*)
+        FROM students
+            GROUP BY birth_year;
+
 
