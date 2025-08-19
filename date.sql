9a328459eadb96cc85024b680b57d0c00e04823a
MD Waliullah <waliullah.godev@gmail.com>
2025-08-19 08:15:35 +0000

    create a timeZ table and insert time with and without timeZone

--- date.sql
+++ date.sql
@@ -1,1 +1,11 @@
 SHOW timezone;
+
+SELECT now();
+
+
+CREATE TABLE timeZ ( ts TIMESTAMP without time zone, tsz TIMESTAMP with time zone );
+
+INSERT INTO timeZ VALUES('2024-01-12 10:45:00', '2024-01-12 10:45:00')
+
+
+SELECT * FROM timeZ;
