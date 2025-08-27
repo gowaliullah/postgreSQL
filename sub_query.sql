a5475e7d2812762fdb576e0a12b7311d49c7beae
MD Waliullah <waliullah.godev@gmail.com>
2025-08-27 16:23:44 +0600

    create employees table with relevant fields

---
+++ sub_query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(50),
+    department_id INT REFERENCES departments(department_id),
+    salary DECIMAL(10, 2),
+    hire_date DATE
+)
